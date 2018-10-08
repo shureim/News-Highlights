024b088efe67f0dd01c17bcea7867d2e2ea9b208
shureim <tarickaliabdi@gmail.com>
2018-10-08 16:31:03 +0300

    created a def test instance class to test if its true

--- test/news_test.py
+++ test/news_test.py
@@ -11,5 +11,5 @@ class NewsTest(unittest.TestCase):
         """
         self.new_news = News("James Reinl", "After trade deal win, Trump doubles down on China", "No plans for China-US negotiations over trade war", "http:\/\/www.aljazeera.com\/news\/2018\/10\/trade-deal-win-trump-doubles-china-181004042022058.html", "https:\/\/www.aljazeera.com\/mritems\/Images\/2018\/8\/29\/80285b02a3b34ae8b31bc78eec9d59a6_18.jpg", "US President Donald Trump's new trade deal with Canada")
 
-    # def test_instance(self):
-    #     self.assertTrue(isinstance(self.new_news,News))
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_news,News))
