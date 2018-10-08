1ce731dddcda3c9f1441acf4e737e455ef32a2a7
shureim <tarickaliabdi@gmail.com>
2018-10-08 16:01:38 +0300

    created the views.py file that will hold the method that returns the index page and data

--- app/main/views.py
+++ app/main/views.py
@@ -1,18 +1,18 @@
-# from flask import render_template
-# from . import main
-# from ..requests import get_news
-#
-# # Views
-# @main.route('/')
-# def index() :
-#     """
-#     view root page function that returns the index page and data
-#     """
-#
-#     sources = get_news('sources')
-#
-#     title = "Now you know "
-#     return render_template('index.html', title = title, sources = sources)
+from flask import render_template
+from . import main
+from ..requests import get_news
+
+# Views
+@main.route('/')
+def index() :
+    """
+    view root page function that returns the index page and data
+    """
+
+    sources = get_news('sources')
+
+    title = "Now you know "
+    return render_template('index.html', title = title, sources = sources)
 #
 # @main.route('/news/<int:news_id>')
 # def news(news_id) :
