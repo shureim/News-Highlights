a3a2befb9ea90e3e95965e24ddcabf828ccd75f3
shureim <tarickaliabdi@gmail.com>
2018-10-08 16:32:20 +0300

    created config.py file for  General configuration of parent class

--- config.py
+++ config.py
@@ -1,18 +1,18 @@
-# import os
-#
-# class Config:
-#     """
-#     General configuration of parent class
-#     """
-#     NEWS_API_BASE_URL = 'https://newsapi.org/v2/{}?apiKey={}'
-#     NEWS_API_TOP_HEADING_URL = 'https://newsapi.org/v2/top-headlines/{}?apiKey={}'
-#     NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
-#     SECRET_KEY = os.environ.get('SECRET_KEY')
-#
-#
-#     pass
-#
-#
+import os
+
+class Config:
+    """
+    General configuration of parent class
+    """
+    NEWS_API_BASE_URL = 'https://newsapi.org/v2/{}?apiKey={}'
+    NEWS_API_TOP_HEADING_URL = 'https://newsapi.org/v2/top-headlines/{}?apiKey={}'
+    NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+
+
+    pass
+
+
 # class ProdConfig(Config):
 #     """
 #     Production configuration child class
