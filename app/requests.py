9dd11bf0a6f94ad40728ff4107588d522a8c2440
shureim <tarickaliabdi@gmail.com>
2018-10-08 16:17:29 +0300

    also imported the api_key and base_url keys

--- app/requests.py
+++ app/requests.py
@@ -10,7 +10,7 @@ base_url =None
 def configure_request(app) :
     global api_key,base_url,article_base_url
     api_key = app.config['NEWS_API_KEY']
-    # base_url = app.config['NEWS_API_BASE_URL']
+    base_url = app.config['NEWS_API_BASE_URL']
     # article_base_url = app.config ['NEWS_API_TOP_HEADING_URL']
 
 # def get_news(category):
